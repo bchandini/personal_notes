b2d4f30fa8a10b1752a16fc98f1378fba3e21e2d
Chandini <mchandini@paypal.com>
2021-02-09 16:17:35 +0530

    comment

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const ejs = require("ejs");
 const mongoose = require('mongoose');
 const homeStartingContent = "Hi! This is Mary Bernard, Welcome to my Journal!";
 const aboutContent = "Hi This is Mary Bernard, I'm a software engineer by profession. This is where I document all of my life's experience. Hope it will help someone to learn from it as much as it has helped me grow";
-const contactContent = "You can drop me an email @ marychandinibernard@gmail.com, would love to hear from you";
+const contactContent = "You can drop me an email at marychandinibernard@gmail.com, would love to hear from you";
 
 const app = express();
 
@@ -20,19 +20,79 @@ mongoose.connect("mongodb+srv://admin-user:Corp2020$@realmcluster.r5zwp.mongodb.
 
 const postSchema = {
   title: String,
-  content: String
+  content: String,
 };
+
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = mongoose.model("User", userSchema);
 
 const Post = mongoose.model("Post", postSchema);
 
 app.get("/", function(req, res){
+  res.render("main");
+});
 
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+
+app.get("/logout", function(req, res){
+  res.redirect("/");
+});
+
+app.get("/home", function(req, res){
   Post.find({}, function(err, posts){
     res.render("home", {
       startingContent: homeStartingContent,
       posts: posts
       });
-  });
+});
+
+app.post("/register", function(req, res){
+
+ const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+ });
+ newUser.save(function(err){
+     if(err){
+         console.log(err);
+     }else{
+      res.redirect("/home");
+      }
+    })
+ })
+
+});
+
+app.post("/login", function(req, res){
+
+    const username= req.body.username;
+    const password= req.body.password;
+
+
+  User.findOne({email: username}, function(err, foundUser){
+    if(err)
+    {
+        console.log(err);
+    }else{
+        if(foundUser){
+            if(foundUser.password === password){
+              res.redirect("/home");              
+            }
+        }
+    }
+ })
+
 });
 
 app.get("/compose", function(req, res){
